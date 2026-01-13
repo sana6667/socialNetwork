81eaf9bf3c62e15fe6a8393d5ff6b20ad7e93945
krasutskaaa <krasutskadiana@gmail.com>
2026-01-13 22:34:51 +0200

    The interfacefor the user class was implemented

--- Interfaces/IUserService.cs
+++ Interfaces/IUserService.cs
@@ -2,5 +2,7 @@ namespace socialNetwork.Services;
 
 public interface IUserService
 {
-    
+    Task<Guid> CreateUserAsync(string username, string password);
+    Task<bool> UsernameExistsAsync(string username);
+    Task<Guid?> GetUserIdByCredentialsAsync(string username, string password);
 }
