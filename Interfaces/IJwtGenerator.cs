803a0a16be3dd3667644f90d32c339d375168d84
krasutskaaa <krasutskadiana@gmail.com>
2026-01-13 23:20:06 +0200

    The interface for the jwt generator service was implemented

--- Interfaces/IJwtGenerator.cs
+++ Interfaces/IJwtGenerator.cs
@@ -1,6 +1,10 @@
+using Microsoft.IdentityModel.Tokens;
+
 namespace socialNetwork.Services;
 
-public class IJwtGenerator
+public interface IJwtGenerator
 {
-    
+    TokenValidationParameters TokenValidationParameters { get; }
+
+    string GenerateToken(Guid userId);
 }
