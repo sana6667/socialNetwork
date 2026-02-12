616161d4c840fa40978b3276286c6e39b6bd0ee8
Oskar <proton2006tom@gmail.com>
2026-02-12 11:20:49 +0200

    started registration page

--- frontend/src/routes/authRoutes.tsx
+++ frontend/src/routes/authRoutes.tsx
@@ -1,10 +1,11 @@
 // src/routes/auth.routes.tsx
 import { type RouteObject } from 'react-router-dom';
+import { Register } from '../pages/Auth/Register';
 
 export const authRoutes: RouteObject[] = [
   {
     path: '/',
-
+    element: <Register />,
   },
   {
     path: '/register',
