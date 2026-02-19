2dbeab3c7007eed94e3491f4471786a48cbb6b0a
sana <sana@loc.com>
2026-02-19 13:21:01 +0200

    fixing errors in an EKS cluster

--- Terraform/module/eks/control-panel/cluster-admin.tf
+++ Terraform/module/eks/control-panel/cluster-admin.tf
@@ -5,6 +5,31 @@ resource "aws_eks_access_entry" "user_access" {
   depends_on = [ aws_eks_cluster.eks_priv_endpoint ]
 }
 
+resource "aws_eks_access_entry" "github_role_access" {
+  cluster_name  = aws_eks_cluster.eks_priv_endpoint.name
+  principal_arn = "arn:aws:iam::336670698191:role/github-terraform-role"
+  type          = "STANDARD"
+
+  depends_on = [
+    aws_eks_cluster.eks_priv_endpoint
+  ]
+}
+
+resource "aws_eks_access_policy_association" "github_role_admin" {
+  cluster_name  = aws_eks_cluster.eks_priv_endpoint.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::336670698191:role/github-terraform-role"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [
+    aws_eks_access_entry.github_role_access,
+    aws_eks_cluster.eks_priv_endpoint
+  ]
+}
+
 resource "aws_eks_access_policy_association" "user_admin" {
   depends_on = [
     aws_eks_cluster.eks_priv_endpoint,
