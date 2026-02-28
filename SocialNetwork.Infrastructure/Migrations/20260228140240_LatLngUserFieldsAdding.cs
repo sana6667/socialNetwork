d2769e5f7e634fe781f5d29d40e9b7f77cb49e98
krasutskaaa <krasutskadiana@gmail.com>
2026-02-28 16:06:45 +0200

    A new migration was implemented as new fields were created in the user entity

---
+++ SocialNetwork.Infrastructure/Migrations/20260228140240_LatLngUserFieldsAdding.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class LatLngUserFieldsAdding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastActiveAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "LatRounded",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "LngRounded",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LocationUpdatedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastActiveAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LatRounded",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LngRounded",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LocationUpdatedAt",
+                table: "AspNetUsers");
+        }
+    }
+}
