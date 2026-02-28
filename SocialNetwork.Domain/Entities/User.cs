9e56a2df0b48cdc0a96f90cee092d9f13d8e8dbe
krasutskaaa <krasutskadiana@gmail.com>
2026-02-28 16:05:01 +0200

    New coordinate fields were added to the user entity

--- SocialNetwork.Domain/Entities/User.cs
+++ SocialNetwork.Domain/Entities/User.cs
@@ -7,6 +7,13 @@ public class User :IdentityUser
     public string Name { get; set; }
     public string ImageUrl { get; set; }
     public string City { get; set; }
+    public double? LatRounded { get; set; }
+    public double? LngRounded { get; set; }
+    public DateTime? LocationUpdatedAt { get; set; }
+
+    //public bool IsDiscoverable { get; set; } = true;
+    
+    public DateTime LastActiveAt { get; set; } = DateTime.UtcNow;
     
     public List<UserInterest> Interests { get; set; } = new ();
     public List<UserPriority> Priorities { get; set; } = new ();
