b6c9248d7b1bc8be1ad9ced78f0409e6eaa3b1ee
sana <sana@loc.com>
2026-03-06 08:51:44 +0200

    fix migrations

--- Terraform/prod/main.tf
+++ Terraform/prod/main.tf
@@ -6,13 +6,7 @@ terraform {
         }
 
     }
-    backend "s3" {
-        bucket = "cdn-buck-sana-556-ss"
-        encrypt = true
-        region = "us-east-1"
-        dynamodb_table = "terraform-up-and-running-locks"
-        key = "global/s3/terraform.tfstate"
-    }
+    
 }
 
 provider "aws" {
