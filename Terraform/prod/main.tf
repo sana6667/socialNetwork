8b2b52b86f8a4f25144a3f516c1f169970185205
sana <sana@loc.com>
2026-03-06 09:13:24 +0200

    add

--- Terraform/prod/main.tf
+++ Terraform/prod/main.tf
@@ -6,6 +6,13 @@ terraform {
         }
 
     }
+    backend "s3" {
+      bucket = "cdn-buck-sana-556-ss"
+      encrypt = true
+      region = "us-east-1"
+      key = "global/s3/terraform.tfstate"
+      dynamodb_table = "terraform-up-and-running-locks"
+    }
     
 }
 
