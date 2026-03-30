2b6918dd7f0ad0b2efb797358e0f5f720a6b41d4
sana <sana@loc.com>
2026-03-30 16:17:02 +0300

    delete

--- app/frontend/src/context/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import { DrawerProvider } from "./DrawerProvider";
-
-export function AppProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <DrawerProvider>
-      {children}
-    </DrawerProvider>
-  );
-}
--- app/frontend/src/pages/Main/Explore/Hangouts/Hangouts.tsx
+++
@@ -1,9 +0,0 @@
-import { CardPeopleItem } from "../../../../assets/CardPeopleItem";
-
-export function Hangouts () {
-  return (
-    <section className="expHang">
-      <CardPeopleItem />
-    </section>
-  );
-};
