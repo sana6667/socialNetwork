04f0c7e04d352cb27a9f85e675fe7da96b48bf99
krasutskaaa <krasutskadiana@gmail.com>
2026-04-01 00:05:24 +0300

    A new ChatAddingUserPriorityPostUpdating migration was added

---
+++ SocialNetwork.Infrastructure/Migrations/20260331210132_ChatAddingUserPriorityPostUpdating.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChatAddingUserPriorityPostUpdating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPriorities");
+
+            migrationBuilder.DropColumn(
+                name: "IsVarified",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartDate",
+                table: "Posts",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "Posts",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Budget",
+                table: "Posts",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Destination",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "From",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriorityId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Chats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsGroup = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatParticipants",
+                columns: table => new
+                {
+                    ChatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatParticipants", x => new { x.UserId, x.ChatId });
+                    table.ForeignKey(
+                        name: "FK_ChatParticipants_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SenderId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SentAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_PriorityId",
+                table: "AspNetUsers",
+                column: "PriorityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatParticipants_ChatId",
+                table: "ChatParticipants",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatId",
+                table: "Messages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SentAt",
+                table: "Messages",
+                column: "SentAt");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Priorities_PriorityId",
+                table: "AspNetUsers",
+                column: "PriorityId",
+                principalTable: "Priorities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Priorities_PriorityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "ChatParticipants");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_PriorityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Budget",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Destination",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "From",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PriorityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartDate",
+                table: "Posts",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "Posts",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVarified",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "UserPriorities",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PriorityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPriorities", x => new { x.UserId, x.PriorityId });
+                    table.ForeignKey(
+                        name: "FK_UserPriorities_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserPriorities_Priorities_PriorityId",
+                        column: x => x.PriorityId,
+                        principalTable: "Priorities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPriorities_PriorityId",
+                table: "UserPriorities",
+                column: "PriorityId");
+        }
+    }
+}
